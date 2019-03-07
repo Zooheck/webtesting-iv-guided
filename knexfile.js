a34629779290de19f6233154cc92eda2a793bff8
Zooheck <davidscottflack@gmail.com>
2019-03-07 12:57:02 -0600

    Configure postgres

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,47 @@
-// Update with your config settings.
-
+// TOP 2 VARIABLES ADDED FOR POSTGRESS
+const localPgConnection = {
+  host: "localhost",
+  database: "hobbits",
+  user: "student",
+  password: "password"
+};
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/hobbits.db3',
+      filename: "./data/hobbits.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
+  //ADDED FOR POSTGRESS
+  production: {
+    client: "pg",
+    connection: prodDbConnection, //an object or a string
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
